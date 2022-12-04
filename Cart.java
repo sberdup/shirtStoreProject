b7d29715e275e9eae434f09185846fc299dd6ca6
Sergio Berdeja <sergioberdeja@gmail.com>
2022-12-03 20:54:00 -0700

    step 7 done

--- Cart.java
+++ Cart.java
@@ -20,23 +20,23 @@ public class Cart {
         if (items.size() == 0){
             System.out.println("Your cart is currently empty. Please add at least one product to check out.");
         } else {
-            System.out.println("Your total is $" + (total * taxRate));
+            String taxTotal = String.format("Your total is $%.2f", (total * (1 + taxRate/100)));
+            System.out.println(taxTotal);
             System.out.println("Thank you for shopping at T-Shirt Mart.");
             empty();
         }
     }
     
     public void showDetails(){
-        System.out.println("showing cart, size: ", items.size());
         if (items.size() == 0){
             System.out.println("The cart is empty. Please add at least one product to see it in the cart.");
         } else {
-            double taxTotal = total * taxRate;
+            double taxTotal = total * (1 + taxRate/100);
             String string1 = String.format("--Cart--%nItem Count: %d%nItems:", items.size());
-            String string2 = String.format("Pre-Tax Total: $%.2f%nPost-Tax Total (%.2f Tax): $%.2f", total, taxRate, taxTotal);
+            String string2 = String.format("%nPre-Tax Total: $%.2f%nPost-Tax Total (%.2f%% Tax): $%.2f", total, taxRate, taxTotal);
             System.out.println(string1);
                 for (Product item : items){
-                    String output = String.format("%s: $%.2d", item.getName(), item.getPrice());
+                    String output = String.format("%s: $%.2f", item.getName(), item.getPrice());
                     System.out.println(output);
                     //System.out.println(item.getName() + ": $"
                 }
